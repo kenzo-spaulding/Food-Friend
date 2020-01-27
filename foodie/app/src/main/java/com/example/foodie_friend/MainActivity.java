eb5d83d0a5e9a26b89d892a0bbd050b17a4d94a5
Kenzo Spaulding <kenzo.spaulding@gmail.com>
2020-01-27 12:33:21 -0800

    adding FireAuth

--- foodie/app/src/main/java/com/example/foodie_friend/MainActivity.java
+++ foodie/app/src/main/java/com/example/foodie_friend/MainActivity.java
@@ -2,11 +2,19 @@ package com.example.foodie_friend;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -14,6 +22,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.time.format.TextStyle;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Button mTest;
@@ -22,20 +32,30 @@ public class MainActivity extends AppCompatActivity {
 
     DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
     DatabaseReference mConditionRef = mRootRef.child("test1");
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.EmailBuilder().build(),
+            new AuthUI.IdpConfig.PhoneBuilder().build(),
+            new AuthUI.IdpConfig.GoogleBuilder().build());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tTest = (TextView)findViewById(R.id.word);
-        zTest = (Button)findViewById(R.id.but);
-        mTest = (Button)findViewById(R.id.test);
+        tTest = (TextView) findViewById(R.id.word);
+        zTest = (Button) findViewById(R.id.but);
+        mTest = (Button) findViewById(R.id.test);
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                1);
     }
 
 
     @Override
-    protected  void onStart() {
+    protected void onStart() {
         super.onStart();
 
         mConditionRef.addValueEventListener(new ValueEventListener() {
@@ -49,7 +69,10 @@ public class MainActivity extends AppCompatActivity {
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
+
         });
+
+
 
         mTest.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,6 +87,28 @@ public class MainActivity extends AppCompatActivity {
                 mConditionRef.setValue("Chips!");
             }
         });
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                // ...
+            }
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                //response.getError().getErrorCode(); //and handle the error.
+                // ...
+        }
     }
 
 
